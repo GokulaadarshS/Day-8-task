02681ea7ca5d9b446435baa5673840a94dd570a4
Gokulaadarsh S <88222086+GokulaadarshS@users.noreply.github.com>
2021-09-02 01:52:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,16 +5,18 @@ class movie{
         this.rating = rating;
     }
     movie_title(){
-        return 'the movie name is ${this.title}';
+        return `the movie name is ${this.title}`;
     }
     movie_studio(){
-        return 'the studio name is ${this.title}';
+        return `the studio name is ${this.studio}`;
     }
     movie_rating(){
-        return 'the rating is ${this.title}';
+        return `the rating is ${this.rating}`;
     }
 }
 const master_obj = new movie("master","xaviour","PG13")
-console.log(master_obj.rating)
+console.log(master_obj.movie_rating())
 const bigil_obj = new movie("bigil","ags","R")
-console.log(bigil_obj.title)
+console.log(bigil_obj.movie_title())
+const casino_obj = new movie("Casino Royale","Eon Productions","PG13")
+console.log(casino_obj.movie_studio())
